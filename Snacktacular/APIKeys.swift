5ab750f06d786693b420184cd3cda9eac48e5682
Derek Marble <dmarble20@gmail.com>
2022-04-03 15:22:21 -0400

    8.7: added Google Places Autocomplete

---
+++ Snacktacular/APIKeys.swift
@@ -0,0 +1,12 @@
+//
+//  APIKeys.swift
+//  Snacktacular
+//
+//  Created by Derek Marble on 4/3/22.
+//
+
+import Foundation
+
+struct APIKeys {
+    static let googlePlacesKey = "AIzaSyBzBCJZMN00dMwxDVTLowwr_vHWgKmYgZE"
+}
