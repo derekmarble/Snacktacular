27dac66df1a2c13fa712af7ff6969c8e15aec7e9
Derek Marble <dmarble20@gmail.com>
2022-04-21 17:14:35 -0400

    8.21: Adding a Photo Picking UIImagePickerController + a Photo Class

--- Snacktacular/PhotoViewController.swift
+++ Snacktacular/PhotoViewController.swift
@@ -6,14 +6,28 @@
 //
 
 import UIKit
+import Firebase
+
+private let dateFormatter: DateFormatter = {
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateStyle = .medium
+    dateFormatter.timeStyle = .none
+    return dateFormatter
+} ()
 
 class PhotoViewController: UIViewController {
 
     @IBOutlet weak var cancelBarButton: UIBarButtonItem!
     @IBOutlet weak var saveBarButton: UIBarButtonItem!
     @IBOutlet weak var deleteBarButton: UIBarButtonItem!
+    @IBOutlet weak var postedByLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var descriptionTextView: UITextView!
+    @IBOutlet weak var photoImageView: UIImageView!
     
     var spot: Spot!
+    var photo: Photo!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -26,9 +40,40 @@ class PhotoViewController: UIViewController {
             print("Error: no spot passed to PhotoViewController.swift")
             return
         }
-
-       
+        
+        if photo == nil {
+            photo = Photo()
+        }
+       updateUserInterface()
     }
+    
+    func updateUserInterface() {
+        postedByLabel.text = "Posted by: \(photo.photoUserEmail)"
+        descriptionTextView.text = photo.description
+        dateLabel.text = "on: \(dateFormatter.string(from: photo.date))"
+        photoImageView.image = photo.image
+        if photo.documentID == "" { //this is a new photo
+            addBordersToEditableObjects()
+        } else {
+            if photo.photoUserID == Auth.auth().currentUser?.uid { // photo posted by the current user
+                self.navigationItem.leftItemsSupplementBackButton = false
+                saveBarButton.title = "Update"
+                addBordersToEditableObjects()
+                self.navigationController?.setToolbarHidden(false, animated: true)
+            } else { // photo added by a different user
+                saveBarButton.hide()
+                cancelBarButton.hide()
+                postedByLabel.text = "Posted by: \(photo.photoUserEmail)"
+                descriptionTextView.isEditable = false
+                descriptionTextView.backgroundColor = .white
+            }
+        }
+    }
+    
+    func addBordersToEditableObjects() {
+        descriptionTextView.addBorder(width: 0.5, radius: 5.0, color: .black)
+    }
+    
     func leaveViewController() {
         let isPresentingInAddMode = presentingViewController is UINavigationController
         if isPresentingInAddMode {
--- Snacktacular/SpotDetailViewController.swift
+++ Snacktacular/SpotDetailViewController.swift
@@ -21,9 +21,11 @@ class SpotDetailViewController: UIViewController {
     
     
     var spot: Spot!
+    var photo: Photo!
     let regionDistance: CLLocationDegrees = 750.0
     var locationManager: CLLocationManager!
     var reviews: Reviews!
+    var imagePickerController = UIImagePickerController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,6 +37,7 @@ class SpotDetailViewController: UIViewController {
         
         tableView.delegate = self
         tableView.dataSource = self
+        imagePickerController.delegate = self
         
         getLocation()
         if spot == nil {
@@ -52,6 +55,11 @@ class SpotDetailViewController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        
+        if spot.documentID != "" {
+            self.navigationController?.setToolbarHidden(true, animated: true)
+        }
+        
         reviews.loadData(spot: spot) {
             self.tableView.reloadData()
         }
@@ -85,6 +93,7 @@ class SpotDetailViewController: UIViewController {
             let navigationController = segue.destination as! UINavigationController
             let destination = navigationController.viewControllers.first as! PhotoViewController
             destination.spot = spot
+            destination.photo = photo
         case "ShowPhoto":
             let destination = segue.destination as! PhotoViewController
 //            TODO: replace code below with collection view code
@@ -125,7 +134,11 @@ class SpotDetailViewController: UIViewController {
                 self.cancelBarButton.hide()
                 self.navigationController?.setToolbarHidden(true, animated: true)
                 self.disableTextEditing()
-                self.performSegue(withIdentifier: segueIdentifier, sender: nil)
+                if segueIdentifier == "AddReview" {
+                    self.performSegue(withIdentifier: segueIdentifier, sender: nil)
+                } else {
+                    self.cameraOrLibraryAlert()
+                }
             }
         }
         let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
@@ -163,6 +176,23 @@ class SpotDetailViewController: UIViewController {
         }
     }
     
+    func cameraOrLibraryAlert() {
+        let alertController = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        let photoLibraryAction = UIAlertAction(title: "Photo Library", style: .default) { _ in
+            self.accessPhotoLibrary()
+        }
+        let cameraAction = UIAlertAction(title: "Camera", style: .default) { _ in
+            self.accessCamera()
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        alertController.addAction(photoLibraryAction)
+        alertController.addAction(cameraAction)
+        alertController.addAction(cancelAction)
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
     
     @IBAction func cancelButtonPressed(_ sender: UIBarButtonItem) {
         leaveViewController()
@@ -186,7 +216,7 @@ class SpotDetailViewController: UIViewController {
         if spot.documentID == "" {
             saveCancelAlert(title: "This Venue has not been Saved", message: "You must save this venue before you can review it", segueIdentifier: "AddPhoto")
         } else {
-            performSegue(withIdentifier: "AddPhoto", sender: nil)
+            cameraOrLibraryAlert()
         }
     }
     
@@ -308,4 +338,36 @@ extension SpotDetailViewController: UITableViewDelegate, UITableViewDataSource {
     }
     
     
+}
+extension SpotDetailViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        photo = Photo()
+        if let editedImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage {
+            photo.image = editedImage
+        } else if let originalImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            photo.image = originalImage
+        }
+        dismiss(animated: true) {
+            self.performSegue(withIdentifier: "AddPhoto", sender: nil)
+        }
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func accessPhotoLibrary() {
+        imagePickerController.sourceType = .photoLibrary
+        present(imagePickerController, animated: true, completion: nil)
+    }
+    
+    func accessCamera() {
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            imagePickerController.sourceType = .camera
+            present(imagePickerController, animated: true, completion: nil)
+        } else {
+            self.oneButtonAlert(title: "Camera Not Available", message: "There is no camera available on this device.")
+        }
+    }
 }
